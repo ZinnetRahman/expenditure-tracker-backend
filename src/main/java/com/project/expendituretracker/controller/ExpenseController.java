af735e0700e3b360c775837e49501fcb4701f78d
Zinnet Rahman <zinnetrahman@gmail.com>
2022-11-17 17:35:08 +0600

    Change database

--- src/main/java/com/project/expendituretracker/controller/ExpenseController.java
+++ src/main/java/com/project/expendituretracker/controller/ExpenseController.java
@@ -5,7 +5,6 @@ import com.project.expendituretracker.service.ExpenseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
