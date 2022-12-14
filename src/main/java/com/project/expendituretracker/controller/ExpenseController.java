ae5755d474fffd77b79783932909199dac9f6fb4
Zinnet Rahman <zinnetrahman@gmail.com>
2022-12-14 15:08:43 +0600

    Removed unwanted code

--- src/main/java/com/project/expendituretracker/controller/ExpenseController.java
+++ src/main/java/com/project/expendituretracker/controller/ExpenseController.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/expense")
 public class ExpenseController {
     private final ExpenseService expenseService;
